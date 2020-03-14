a33d5565df066a95416a0d87458dd15418e9853b
song <1366237319@qq.com>
2020-03-14 19:47:04 +0800

    add sql ddl

--- src/main/java/com/song/community/controller/AuthorizeController.java
+++ src/main/java/com/song/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.song.community.controller;
 
 import com.song.community.dto.AccessTokenDTO;
 import com.song.community.dto.GithubUser;
+import com.song.community.mapper.UserMapper;
+import com.song.community.model.User;
 import com.song.community.provide.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,11 +11,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
 @Controller
 public class AuthorizeController {
 
     @Autowired
-    public GithubProvider githubProvider;
+    private GithubProvider githubProvider;
+
+    @Autowired UserMapper userMapper;
 
     @Value("${github.client.id}")
     private String clientId;
@@ -26,7 +33,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
-                           @RequestParam(name = "state")String state){
+                           @RequestParam(name = "state")String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setClient_id(clientId);
@@ -34,8 +42,19 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_url(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessTokenDTO(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
+        } else {
+            return "redirect:/";
+        }
     }
 }
--- src/main/java/com/song/community/controller/IndexController.java
+++ src/main/java/com/song/community/controller/IndexController.java
@@ -13,5 +13,7 @@ public class IndexController {
         return "index";
     }
 
+
+
 }
 
