7ac6f3bfbd1bed827371dfb5831b63eb300d812d
song <1366237319@qq.com>
2020-03-14 20:43:50 +0800

    add check cookies

--- src/main/java/com/song/community/controller/AuthorizeController.java
+++ src/main/java/com/song/community/controller/AuthorizeController.java
@@ -5,13 +5,16 @@ import com.song.community.dto.GithubUser;
 import com.song.community.mapper.UserMapper;
 import com.song.community.model.User;
 import com.song.community.provide.GithubProvider;
+im
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -34,7 +37,7 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
                            @RequestParam(name = "state")String state,
-                           HttpServletRequest request){
+                           HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setClient_id(clientId);
@@ -45,13 +48,17 @@ public class AuthorizeController {
         GithubUser githubUser = githubProvider.getUser(accessToken);
         if (githubUser != null){
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
-            request.getSession().setAttribute("user",githubUser);
+
+            response.addCookie(new Cookie("token",token));
+
+
             return "redirect:/";
         } else {
             return "redirect:/";
--- src/main/java/com/song/community/mapper/UserMapper.java
+++ src/main/java/com/song/community/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package com.song.community.mapper;
 import com.song.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 
@@ -12,7 +13,6 @@ public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified}) ")
     void insert(User user);
 
-    @Select("select * from user")
-    void Select(User user);
-
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);  //@Param意思是也可以放类,通过类获取token
 }
